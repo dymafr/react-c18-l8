a614de31eabd8848ca5a1965c63d355f496611d6
dymafr <32801166+dymafr@users.noreply.github.com>
2022-10-12 18:21:31 +0200

    1 file modified, 1 added

--- src/pages/Admin/pages/AdminRecipes/components/AdminRecipesNav/AdminRecipesNav.js
+++ src/pages/Admin/pages/AdminRecipes/components/AdminRecipesNav/AdminRecipesNav.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import styles from './AdminRecipesNav.module.scss';
 
 function AdminRecipesNav() {
-  return <h4>AdminRecipesNav</h4>;
+  return (
+    <ul className={styles.list}>
+      <NavLink
+        className={({ isActive }) => (isActive ? styles.active : '')}
+        to="list"
+      >
+        Liste des recettes
+      </NavLink>
+      <NavLink
+        className={({ isActive }) => (isActive ? styles.active : '')}
+        to="new"
+      >
+        Ajouter une recette
+      </NavLink>
+    </ul>
+  );
 }
 
 export default AdminRecipesNav;
