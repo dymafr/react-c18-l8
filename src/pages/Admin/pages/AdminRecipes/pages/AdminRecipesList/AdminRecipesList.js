3cff00c55e9255f758fa27a7771dc57678f685d0
dymafr <32801166+dymafr@users.noreply.github.com>
2022-10-12 18:29:25 +0200

    2 files modified

--- src/pages/Admin/pages/AdminRecipes/pages/AdminRecipesList/AdminRecipesList.js
+++ src/pages/Admin/pages/AdminRecipes/pages/AdminRecipesList/AdminRecipesList.js
@@ -1,16 +1,9 @@
 import React from 'react';
 import { useFetchRecipes } from '../../../../../../hooks';
 import styles from './AdminRecipesList.module.scss';
-import { deleteRecipe as deleteR } from '../../../../../../apis';
-import { NavLink } from 'react-router-dom';
 
 function AdminRecipesList() {
-  const [[recipes, setRecipes]] = useFetchRecipes();
-
-  async function deleteRecipe(_id) {
-    await deleteR(_id);
-    setRecipes(recipes.filter((r) => r._id !== _id));
-  }
+  const [[recipes]] = useFetchRecipes();
 
   return (
     <ul className={styles.list}>
@@ -18,15 +11,8 @@ function AdminRecipesList() {
         ? recipes.map((r) => (
             <li key={r._id} className="d-flex align-items-center">
               <span className="flex-fill">{r.title}</span>
-              <NavLink to={`../edit/${r._id}`}>
-                <button className="btn btn-primary mr-15">Editer</button>
-              </NavLink>
-              <button
-                onClick={() => deleteRecipe(r._id)}
-                className="btn btn-danger"
-              >
-                Supprimer
-              </button>
+              <button className="btn btn-primary mr-15">Editer</button>
+              <button className="btn btn-danger">Supprimer</button>
             </li>
           ))
         : null}
